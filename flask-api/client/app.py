68b8d43cd82bf05ae76722976a7e637e34c935a1
zzzzzhy <11657924@qq.com>
2023-09-26 04:32:51 +0800

    test

--- flask-api/client/app.py
+++ flask-api/client/app.py
@@ -1,12 +1,30 @@
 import warnings
 from collections import OrderedDict
-
+import os
+import json
 import flwr as fl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torchvision.transforms import Compose, Normalize, ToTensor
 from src.temper_forecast import train, load_data, test, DNN
+import requests
+
+from eth_account.account import Account
+from console_utils.cmd_account import CmdAccount
+from client_config import client_config
+
+curdir = os.path.dirname(os.path.abspath(__file__))
+contracts_dir = "contracts"
+import uuid
+def get_mac_address():
+    """
+    获取本机物理地址，获取本机mac地址
+    :return:
+    """
+    mac=uuid.UUID(int = uuid.getnode()).hex[-12:].upper()
+    # return 'pyaccount'
+    return "-".join([mac[e:e+2] for e in range(0,11,2)])
 
 # #############################################################################
 # 1. Regular PyTorch pipeline: nn.Module, train, test, and DataLoader
@@ -49,6 +67,16 @@ class FlowerClient(fl.client.NumPyClient):
 
 if __name__ == "__main__":
     # Start Flower client
+    key_file = "{}/{}".format(client_config.account_keyfile_path, client_config.account_keyfile)
+    if not os.access(key_file, os.F_OK):
+        CmdAccount.create_ecdsa_account(get_mac_address(),client_config.account_password)
+        
+    with open(key_file, "r") as dump_f:
+        keytext = json.load(dump_f)
+        privkey = Account.decrypt(keytext, client_config.account_password)
+        ac2 = Account.from_key(privkey)
+        res = requests.post(client_config.node+'/register',json={'address':ac2.address})
+        print("register:\t", res.text)
     fl.client.start_numpy_client(
         server_address="flwr-server:8080",
         client=FlowerClient(),
